67abe3ba9e8fc71c4ce45220180a79a73dd03300
BobiOneKenobi <brainblessed95@gmail.com>
2016-10-02 01:21:13 +0300

    HEHE

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-
-
-
-
 class Program
 {
     static void Main(string[] args)
@@ -13,7 +9,6 @@ class Program
         var hours = days * 24;
         var minutes = hours * 60;
         Console.WriteLine("{0} centuries = {1} years = {2} days = {3} hours = {4} minutes", centuries, years, days, hours, minutes);
-
     }
 }
 
